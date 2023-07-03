475ef61f8ccaff315378b4603cb3e12d1489dc4e
Akshayshinde123456 <shindeakshay1257@gmail.com>
2023-07-03 17:15:50 +0530

    Files Added Successfully

---
+++ src/main/java/com/akshay/ItemsApi/service/ItemService.java
@@ -0,0 +1,18 @@
+package com.akshay.ItemsApi.service;
+
+import com.akshay.ItemsApi.repository.entity.Item;
+
+import java.util.List;
+
+public interface ItemService
+{
+
+    Item save( Item item );
+
+    void delete( int itemId );
+
+    List<Item> all();
+
+    Item findById( int itemId );
+
+}
